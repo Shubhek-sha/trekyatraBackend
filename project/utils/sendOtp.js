899b2d8feb614992781953c282bc9b99050099b3
Sunil Shrestha <suneelshrestha9@gmail.com>
2026-04-07 23:08:37 +0545

    ipv4 for nodemailer

--- project/utils/sendOtp.js
+++ project/utils/sendOtp.js
@@ -8,6 +8,8 @@ export const sendOtp = async (email, otp) => {
     const transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
       port: 587,
+      secure: false,
+      family: 4,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
