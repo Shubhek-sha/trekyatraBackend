082ebe1571f5bf1d13d7a0af24b736ff16f61415
Sunil Shrestha <suneelshrestha9@gmail.com>
2026-04-08 22:59:34 +0545

    feat: added model recommendatoin

--- project/routes/trek.routes.js
+++ project/routes/trek.routes.js
@@ -1,15 +1,15 @@
 // routes/trek.routes.js
 
 import {Router} from 'express';
-import {createTrek, getAllTreks, getTrekById, updateTrek, deleteTrek, updateItinerary} from '../controllers/trek.controller.js'; // <-- .js extension required for ESM
+import {createTrek, getAllTreks, getTrekById, updateTrek, deleteTrek, updateItinerary} from '../controllers/trek.controller.js';
 
 const router = Router();
 
-router.get('/', getAllTreks); // GET    /api/treks
-router.post('/', createTrek); // POST   /api/treks
-router.get('/:id', getTrekById); // GET    /api/treks/:id
-router.patch('/:id', updateTrek); // PATCH  /api/treks/:id
-router.delete('/:id', deleteTrek); // DELETE /api/treks/:id
-router.patch('/:id/itinerary', updateItinerary); // PATCH  /api/treks/:id/itinerary
+router.get('/', getAllTreks);
+router.post('/', createTrek);
+router.get('/:id', getTrekById);
+router.patch('/:id', updateTrek);
+router.delete('/:id', deleteTrek);
+router.patch('/:id/itinerary', updateItinerary);
 
 export default router;
